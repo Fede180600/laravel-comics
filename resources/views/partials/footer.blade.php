6c2be495ad2325f0a571a814d279adad172a5d19
Fede180600 <fede180600@libero.it>
2022-06-26 12:58:47 +0200

    footer style complete

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,27 @@
 <footer>
-    FOOTER
+    <div class="footer-top">
+        <div class="col-text">
+        
+        </div>
+        <div class="col-logo">
+            <img src="../images/dc-logo-bg.png" alt="">
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="btn-box">
+                <button>
+                    sing-up now!
+                </button>
+            </div>
+            <ul>
+                <h2>follow us</h2>
+                @foreach ($socials_array as $social)
+                    <li>
+                        <img src="{{ $social['icon_url'] }}" alt="">
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 </footer>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,23 @@ Route::get('/', function () {
     $comics_array = config('comics');
     $data = [
         'comics_array' => $comics_array,
+        'socials_array' => [
+            [
+                'icon_url' => '../images/footer-facebook.png'
+            ],
+            [
+                'icon_url' => '../images/footer-periscope.png'
+            ],
+            [
+                'icon_url' => '../images/footer-pinterest.png'
+            ],
+            [
+                'icon_url' => '../images/footer-twitter.png'
+            ],
+            [
+                'icon_url' => '../images/footer-youtube.png'
+            ]
+            ],
     ];
     return view('home', $data);
 })->name('home');
