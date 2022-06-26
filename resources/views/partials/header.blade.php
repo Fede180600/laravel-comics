bb4090d7a162da342bd5c04514082a663d377e1e
Fede180600 <fede180600@libero.it>
2022-06-26 12:15:34 +0200

    header complete

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,35 @@
 <header>
-    HEADER
+    <div class="header-top">
+        <div>
+            <span>Dc power visa</span>
+            <select name="" id="">
+                <option value="">additional DC sites</option>
+            </select>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="container">
+            <div class="logo-img">
+                <img src="../images/dc-logo.png" alt="">
+            </div>
+            <nav>
+                <ul>
+                    <li>characheters</li>
+                    <li class="active">comics</li>
+                    <li>movies</li>
+                    <li>tv</li>
+                    <li>games</li>
+                    <li>collectibles</li>
+                    <li>videos</li>
+                    <li>fans</li>
+                    <li>news</li>
+                    <li>shop</li>
+                </ul>
+            </nav>
+            <div class="search-box">
+                <input type="search" placeholder="Search">
+
+            </div>
+        </div>
+    </div>
 </header>
