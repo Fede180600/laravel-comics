e5f50bcba56ebd4b3b36dce50aa9853927974e08
Fede180600 <fede180600@libero.it>
2022-06-27 16:35:34 +0200

    print all comic[i] info

--- resources/views/components/comic-description.blade.php
+++ resources/views/components/comic-description.blade.php
@@ -1,17 +1,34 @@
 <section class="comic-section">
     <div class="blue-line">
         <div class="container">
-
+            <img src="{{ $comic['thumb'] }}" alt="">
         </div>
     </div>
     <div class="overview-box">
         <div class="container">
-
+            <h2>{{ $comic['title'] }}</h2>
+            <div class="price-box">
+                {{ $comic['price'] }}
+            </div>
+            <p>
+                {{ $comic['description'] }}
+            </p>
         </div>
     </div>
     <div class="info-box">
         <div class="container">
-            
+            @foreach ($comic['artists'] as $name)
+               Art by: {{ $name }},
+            @endforeach
+            @foreach ($comic['writers'] as $name)
+                Written by: {{ $name }},
+            @endforeach
+            <br>
+            {{ $comic['series'] }}
+            <br>
+            {{ $comic['price'] }}
+            <br>
+            {{ $comic['sale_date'] }}
         </div>
     </div>
 </section>
