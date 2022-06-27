0a8046c3283221ed1c85ecde21d24c786b5f047a
Fede180600 <fede180600@libero.it>
2022-06-27 18:18:31 +0200

    add style at comic info section

--- resources/views/components/comic-description.blade.php
+++ resources/views/components/comic-description.blade.php
@@ -31,18 +31,44 @@
     </div>
     <div class="info-box">
         <div class="container">
-            @foreach ($comic['artists'] as $name)
-               Art by: {{ $name }},
-            @endforeach
-            @foreach ($comic['writers'] as $name)
-                Written by: {{ $name }},
-            @endforeach
-            <br>
-            {{ $comic['series'] }}
-            <br>
-            {{ $comic['price'] }}
-            <br>
-            {{ $comic['sale_date'] }}
+
+            <div class="col-left">
+                <h3>Talent</h3>
+                <div>
+                    <span>Art by:</span> 
+                    @foreach ($comic['artists'] as $name)
+                        {{ $name }},
+                    @endforeach
+                </div>
+
+                <div>
+                    <span>Written by:</span>
+                    @foreach ($comic['writers'] as $name)
+                        {{ $name }},
+                    @endforeach
+                </div>
+            </div>
+
+            <div class="col-right">
+                <h3>Specs</h3>
+                <div style="text-transform: uppercase; color: #21a9dd; font-weight: 600;">
+                    <p style="color: #00375f; text-transform: capitalize;">Series:</p>
+                    {{ $comic['series'] }}
+                </div>
+                
+
+                <div>
+                    <p>U.S. Price:</p>
+                    {{ $comic['price'] }}
+                </div>
+                
+
+                
+                <div>
+                    <p>On Sale Date</p>
+                    {{ $comic['sale_date'] }}
+                </div>
+            </div>
         </div>
     </div>
 </section>
